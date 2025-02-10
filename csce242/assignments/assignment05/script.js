da1183e2a915f7d39e2af757c4d7afb206c4a735
olivia-casper <oliviaanncasper@gmail.com>
2025-02-10 07:50:10 -0500

    name

--- csce242/assignments/assignment05/script.js
+++ csce242/assignments/assignment05/script.js
@@ -1,5 +1,4 @@
-// Say Hello Feature
-
+// Say Hello
 document.getElementById("hello-box").onclick = () => {
     console.log("Box clicked!");
     document.getElementById("hello-output").innerHTML += "<p>hello</p>";
@@ -11,7 +10,7 @@ document.getElementById("color-picker").oninput = () => {
     document.getElementById("star").style.color = document.getElementById("color-picker").value;
 };
 
-// Image Change Feature
+// Image Change 
 const images = ["images/goodmorning1.webp", "images/goodafternoon2.jpg", "images/goodnight3.jpg"];
 let currentImage = 0;
 
@@ -19,12 +18,12 @@ document.getElementById("dynamic-image").onclick = () => {
     console.log("Image clicked!");
     currentImage++;
     if (currentImage >= images.length) {
-        currentImage = 0; // Reset index when it reaches the end
+        currentImage = 0; 
     }
     document.getElementById("dynamic-image").src = images[currentImage];
 };
 
-// Footer message interaction
+// Footer message 
 document.getElementById("footer-fun").onmouseover = () => {
     console.log("Footer clicked!");
     document.getElementById("footer-fun").innerHTML = "Thanks for visiting!";
