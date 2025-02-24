1776a51784b131a4c44e377a2dc59be92322bba5
olivia-casper <oliviaanncasper@gmail.com>
2025-02-23 23:16:26 -0500

    disable

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -2,24 +2,25 @@ const steps = 10;
 const rate = 31;
 
 const img = document.querySelector("img");
-const climb = document.getElementById("climb-button")
+const climb = document.getElementById("climb-button");
+const draw = document.getElementById("draw-button");
 
 const createStairs = () => {
     const stairs = document.getElementById("stairs");
     stairs.classList.add("border-inline");
     for (let i = 0; i < steps; ++i) {
-      const hr = document.createElement("hr");
-      stairs.append(hr);
+        const hr = document.createElement("hr");
+        stairs.append(hr);
     }
   };
 
-const draw = document.getElementById("draw-button");
 draw.onclick = () => {
     createStairs();
     img.classList.remove("hidden");
     climb.classList.remove("hidden");
-  };
 
+    draw.disabled = true;
+};
 
 let counter = 0;
 let stepsClimbed = 0;
@@ -27,18 +28,20 @@ let intervalId;
 const array = ["./images/right.png", "./images/left.png"];
 
 const Climb = () => {
-  img.setAttribute("src", array[counter]);
-  const bottom = Number.parseInt(getComputedStyle(img).bottom);
-  img.style.bottom = `${bottom + rate}px`;
+    img.setAttribute("src", array[counter]);
+    const bottom = Number.parseInt(getComputedStyle(img).bottom);
+    img.style.bottom = `${bottom + rate}px`;
 
-  ++counter;
-  if (counter === 2) counter = 0;
+    ++counter;
+    if (counter === 2) counter = 0;
 
-  ++stepsClimbed;
-  if (stepsClimbed === steps + 1) clearInterval(intervalId);
+    ++stepsClimbed;
+    if (stepsClimbed === steps + 1) clearInterval(intervalId);
 };
 
 climb.onclick = () => {
-  intervalId = setInterval(Climb, 1000);
+    intervalId = setInterval(Climb, 1000);
+
+    climb.disabled = true;
 };
 
