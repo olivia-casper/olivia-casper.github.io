f09bf1f6daafa3c67482523bda9dd334aa1d1816
olivia-casper <oliviaanncasper@gmail.com>
2025-02-23 22:57:13 -0500

    assignment07

---
+++ csce242/assignments/assignment07/script.js
@@ -0,0 +1,44 @@
+const steps = 10;
+const rate = 31;
+
+const img = document.querySelector("img");
+const climb = document.getElementById("climb-button")
+
+const createStairs = () => {
+    const stairs = document.getElementById("stairs");
+    stairs.classList.add("border-inline");
+    for (let i = 0; i < steps; ++i) {
+      const hr = document.createElement("hr");
+      stairs.append(hr);
+    }
+  };
+
+const draw = document.getElementById("draw-button");
+draw.onclick = () => {
+    createStairs();
+    img.classList.remove("hidden");
+    climb.classList.remove("hidden");
+  };
+
+
+let counter = 0;
+let stepsClimbed = 0;
+let intervalId;
+const array = ["./images/right.png", "./images/left.png"];
+
+const Climb = () => {
+  img.setAttribute("src", array[counter]);
+  const bottom = Number.parseInt(getComputedStyle(img).bottom);
+  img.style.bottom = `${bottom + rate}px`;
+
+  ++counter;
+  if (counter === 2) counter = 0;
+
+  ++stepsClimbed;
+  if (stepsClimbed === steps + 1) clearInterval(intervalId);
+};
+
+climb.onclick = () => {
+  intervalId = setInterval(Climb, 1000);
+};
+
